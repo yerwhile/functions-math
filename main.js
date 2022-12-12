b068ad740bed7a76805ae7d48ea6b6bad39146bd
Matthew McDonald <yerwhile@gmail.com>
2022-12-12 09:38:23 -0600

    functions math

---
+++ main.js
@@ -0,0 +1,13 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber;
+    return sum;
+}
+
+const doubleNum = (num) => {
+    const doubled = num * 2;
+    return doubled;
+}
+
+addSum = add(4,14);
+multDouble = doubleNum(addSum);
+console.log(multDouble);
